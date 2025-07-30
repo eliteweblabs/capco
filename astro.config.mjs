dedcada5e4d603b7729fdde615a66b329d666c2e
Thomas Senecal <sen@eliteweblabs.com>
2025-07-30 15:51:03 -0400

    Fix: Switch to Netlify adapter for SSR deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,15 +15,13 @@
 
 // @ts-check
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
   integrations: [tailwind()],
   i18n: {
     defaultLocale: "en",
