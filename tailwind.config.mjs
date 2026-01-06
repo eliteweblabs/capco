21af07183daac45733140fefecd77f3a5020de76
Thomas Senecal <sen@eliteweblabs.com>
2026-01-05 19:06:10 -0500

    Update Tailwind CSS configuration and components: add z-index utility, adjust layout classes in App.astro, implement radial gradient backgrounds in LandingProductCapco.astro, and refine global styles for improved visual consistency. This enhances the overall UI and responsiveness of the application.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -77,6 +77,7 @@ export default {
       },
       zIndex: {
         '-1': '-1',
+        '1': '1',
       },
       animation: {
         'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
